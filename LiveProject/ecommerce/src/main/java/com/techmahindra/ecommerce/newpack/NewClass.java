0c2c43107d1a0ebc528523066aa0f4aab03a783d
Kalagara Manoj Kumar <manojkumar.kalagara25@gmail.com>
2022-10-30 14:00:11 +0530

    I Created new class under new pack

---
+++ LiveProject/ecommerce/src/main/java/com/techmahindra/ecommerce/newpack/NewClass.java
@@ -0,0 +1,10 @@
+package com.techmahindra.ecommerce.newpack;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("New Class Created under new pack");
+	}
+
+}
