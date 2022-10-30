2b9886c926e3797d798edeb37f76619a92f2fdc6
Kalagara Manoj Kumar <manojkumar.kalagara25@gmail.com>
2022-10-30 14:04:23 +0530

    Added one more line

--- LiveProject/ecommerce/src/main/java/com/techmahindra/ecommerce/newpack/NewClass.java
+++ LiveProject/ecommerce/src/main/java/com/techmahindra/ecommerce/newpack/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("New Class Created under new pack");
+System.out.println("Added one more Line");
 	}
 
 }
